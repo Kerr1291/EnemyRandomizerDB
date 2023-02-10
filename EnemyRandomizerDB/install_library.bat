5bfd59eda4983c294d461c383713fa1264a4ba57
kerr1291 <libra.ae@gmail.com>
2023-02-10 14:15:04 -0800

    Fixed copy paths

--- EnemyRandomizerDB/install_library.bat
+++ EnemyRandomizerDB/install_library.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Installing Library
-SET DLL_SOURCE="..\EnemyRandomizerDB\bin\Library\EnemyRandomizerDB.dll"
+SET DLL_SOURCE="..\..\..\EnemyRandomizerDB\bin\Library\EnemyRandomizerDB.dll"
 SET MOD_DEST="K:\SteamLibrary\steamapps\common\Hollow Knight\hollow_knight_Data\Managed\Mods\EnemyRandomizer"
 echo Copying build from
 echo %DLL_SOURCE%
 echo to
 echo %MOD_DEST%
 copy %DLL_SOURCE% %MOD_DEST%
-SET XML_SOURCE="..\EnemyRandomizerDB\Library\XMLResources\EnemyRandomizerDatabase.xml"
+SET XML_SOURCE="..\..\..\EnemyRandomizerDB\Library\XMLResources\EnemyRandomizerDatabase.xml"
 SET MOD_DEST="K:\SteamLibrary\steamapps\common\Hollow Knight\hollow_knight_Data\Managed\Mods\EnemyRandomizer"
 echo Copying XML database from
 echo %XML_SOURCE%
